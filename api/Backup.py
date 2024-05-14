03516fed4874459488a109c6918d555a3ba4269b
星痕Sky <starrytracesky@qq.com>
2024-05-14 13:51:45 +0800

    fix: 修复初始化的问题

--- api/Backup.py
+++ api/Backup.py
@@ -12,16 +12,18 @@ import subprocess
 from time import strftime
 
 import pysnooper
+from pathlib import Path
+from ShareAndTalk.settings import BASE_DIR
 
 
 class Backuper:
     def __backupDB(self):
         """Backup share and talk database"""
         # Export database as backup.json
-        date = strftime('%Y_%m_%d_%H_%M_%S')
-        filepath = f'backup/backup_{date}.json'
+        date = strftime('%Y%m%d_%H%M%S')
+        filePath = Path(BASE_DIR) / f'backup/backup_{date}.json'
         args = ['py', '-Xutf8', 'manage.py', 'dumpdata', '-o',
-                filepath]
+                filePath.absolute().resolve()]
         # py -Xutf8 manage.py dumpdata -o backup_2023_06_06.json
         subprocess.check_call(args, creationflags=subprocess.CREATE_NO_WINDOW)
 
