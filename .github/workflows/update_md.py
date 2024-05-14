407065802caf6cde51942e9c288dad635fd0af11
星痕Sky <starrytracesky@qq.com>
2024-05-14 18:06:01 +0800

    fix: action

--- .github/workflows/update_md.py
+++ .github/workflows/update_md.py
@@ -49,7 +49,6 @@ def updateMD(file):
     if '-' in tagName:
         tagName = tagName.replace('-', '_')
 
-    tagName = '-' + tagName + '-'
     Content = re.sub(pattern, tagName, Content)
 
     # update REAMDE.md
